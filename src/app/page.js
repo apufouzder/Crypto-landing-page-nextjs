3a128775373ed0b18e9c73a8b97738f3c9395e86
apufouzder <apufouzdar@gmail.com>
2024-01-20 06:47:24 -0800

    Service section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Navbar from '@/components/Navbar'
+import Services from '@/components/Services'
 import Sponsor from '@/components/Sponsor'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
+    <main className='px-4 md:px-0'>
       <Navbar />
       <Sponsor />
-    </>
+      <Services />
+    </main>
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,50 @@
+"use client"
+import React from 'react';
+import { GiWallet } from "react-icons/gi";
+
+
+const data = [
+    {
+        id: 1,
+        title: "Connect wallet & exchanges",
+        desc: "Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut volup.",
+        icon: <GiWallet className='mx-auto' />,
+    },
+    {
+        id: 2,
+        title: "Connect wallet & exchanges",
+        desc: "Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut volup.",
+        icon: <GiWallet className='mx-auto' />,
+    },
+    {
+        id: 3,
+        title: "Connect wallet & exchanges",
+        desc: "Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut volup.",
+        icon: <GiWallet className='mx-auto' />,
+    },
+];
+
+const Services = () => {
+
+    return (
+        <div className='container mx-auto py-20'>
+            <div className='text-center'>
+                <h1 className='text-6xl font-medium mb-8'>Automated portfolio tracking</h1>
+                <p className='text-secondary text-lg mb-8'>Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint <br /> laboriosam sed ipsa sint ut voluptatum labore et possimus voluptas.</p>
+            </div>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
+                {data.map((item) => (
+                    <div key={item.id} className='px-6 py-8 rounded-lg border border-primary text-center hover:translate-y-2 transition-all duration-500'>
+                        <span className='text-7xl pt-6 block'>
+                            {item.icon}
+                        </span>
+                        <h2 className='text-3xl font-semibold my-6'>{item.title}</h2>
+                        <p className='text-base font-normal'>{item.desc}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Services;
